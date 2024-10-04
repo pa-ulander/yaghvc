2f2fb8f2b240d30d551ea0e04d7bf9ba397beaa9
PA Ulander <pa@kabelkultur.se>
2024-10-04 16:24:00 +0200

    use named parameters

--- tests/Unit/ProfileViewTest.php
+++ tests/Unit/ProfileViewTest.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 use App\Models\ProfileViews;
 
-it('creates a new profile view', function () {
-    $profileView = ProfileViews::create(['username' => 'testuser']);
+it(description: 'creates a new profile view', closure: function (): void {
+    $profileView = ProfileViews::create(attributes: ['username' => 'testuser']);
 
-    expect($profileView->username)->toBe('testuser');
+    expect(value: $profileView->username)->toBe(expected: 'testuser');
 });
 
-it('checks if a username has been added to the database', function () {
-    ProfileViews::create(['username' => 'testuser']);
+it(description: 'checks if a username has been added to the database', closure: function (): void {
+    ProfileViews::create(attributes: ['username' => 'testuser']);
 
-    $profileView = ProfileViews::where('username', 'testuser')->first();
+    $profileView = ProfileViews::where(column: 'username', operator: 'testuser')->first();
 
-    expect($profileView)->not()->toBeNull();
-    expect($profileView->username)->toBe('testuser');
+    expect(value: $profileView)->not()->toBeNull();
+    expect(value: $profileView->username)->toBe(expected: 'testuser');
 });
