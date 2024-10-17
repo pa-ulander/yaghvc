24244082337cf38506eb7160ec40cd243ae70375
PA Ulander <pa@kabelkultur.se>
2024-10-17 19:09:38 +0200

    refactored some

--- app/Models/ProfileViews.php
+++ app/Models/ProfileViews.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class ProfileViews extends Model
 {
@@ -33,7 +34,7 @@ class ProfileViews extends Model
 
     public function getCount($username)
     {
-        return Cache::remember(key: 'count-'.$username, ttl: 1, callback: function () use ($username): int {
+        return Cache::remember(key: 'count-' . $username, ttl: 1, callback: function () use ($username): int {
             $profileView = self::where(column: 'username', operator: $username)->first();
             return $profileView->username ? $profileView->visit_count : 0;
         });
@@ -41,12 +42,14 @@ class ProfileViews extends Model
 
     public function incrementCount()
     {
-        if ($this->username) {
-            $this->visit_count++;
-            $this->last_visit = now();
-            $this->save();
-        } else {
-            throw new \Exception('Username is missing for this instance.');
+        if (!$this->username) {
+            Log::error('Attempt to increment count for ProfileView without username', ['id' => $this->id]);
+            throw new \InvalidArgumentException('Username is missing for this instance.');
         }
+    
+        $this->increment('visit_count');
+        $this->update(['last_visit' => now()]);
+    
+        Cache::forget("count-{$this->username}");
     }
 }
