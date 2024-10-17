b7542de6d8e78485489f149a4c0cbc01c1179a1f
PA Ulander <pa@kabelkultur.se>
2024-10-17 19:10:02 +0200

    refactored some

---
+++ app/Repositories/ProfileViewsRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\Repositories;
+
+use App\Models\ProfileViews;
+
+class ProfileViewsRepository
+{
+    public function findOrCreate(string $username): ProfileViews
+    {
+        $profileView = ProfileViews::firstOrCreate(
+            attributes: ['username' => $username],
+            values: ['visit_count' => 0, 'last_visit' => now()]
+        );
+        
+        $profileView->incrementCount();
+
+        return $profileView;
+    }
+}
