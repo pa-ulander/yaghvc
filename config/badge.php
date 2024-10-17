1fcadd1f54d18c9224f5c30907cd4923bd75f6f2
PA Ulander <pa@kabelkultur.se>
2024-10-17 19:10:21 +0200

    add config

---
+++ config/badge.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Defaults for badge render
+ */
+return [
+    'default_label' => 'Visits',
+    'default_color' => 'blue',
+    'default_style' => 'for-the-badge',
+    'default_base' => 0,
+    'default_abbreviated' => false,
+];
