241434eaf56201002bc3f2156e91f7bb9ff25585
PA Ulander <pa@kabelkultur.se>
2024-10-17 19:09:57 +0200

    refactored some

--- app/Http/Controllers/ProfileViewsController.php
+++ app/Http/Controllers/ProfileViewsController.php
@@ -5,34 +5,49 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileViewRequest;
+use App\Repositories\ProfileViewsRepository;
 use App\Models\ProfileViews;
 use App\Services\BadgeRenderService;
+use Illuminate\Http\Response;
+use Illuminate\Contracts\Routing\ResponseFactory;
 
 class ProfileViewsController extends Controller
 {
-    public function index(ProfileViewRequest $request)
+    public function __construct(
+        private BadgeRenderService $badgeRenderService,
+        private ProfileViewsRepository $profileViewsRepository
+    ) {
+        $this->badgeRenderService = $badgeRenderService;
+        $this->profileViewsRepository = $profileViewsRepository;
+    }
+
+    public function index(ProfileViewRequest $request): ResponseFactory|Response
     {
-        $profileView = ProfileViews::where(column: 'username', operator: $request->username)->first();
+        $safe = $request->safe();
+        $profileView = $this->profileViewsRepository->findOrCreate(username: $safe->username);
 
-        if ($profileView?->username) {
-            $profileView->incrementCount();
-        } else {
-            $profileView = ProfileViews::create(attributes: ['username' => $request->username, 'visit_count' => 1, 'last_visit' => now()]);
-        }
-        
-        $badgeRenderService = new BadgeRenderService;
-        
-        $badgeRender = $badgeRenderService->renderBadgeWithCount(
-            label: $request->getBadgeLabel() ?? 'Views',
-            count: $profileView->getCount($request->username) ?? 0,
-            messageBackgroundFill: $request->color ?? 'green',
-            badgeStyle: $request->getBadgeStyle() ?? 'for-the-badge',
-            abbreviated: $request?->getAbbreviated() ?? false
+        $badgeRender = $this->renderBadge(safe: $safe, profileView: $profileView);
+
+        return $this->createBadgeResponse(badgeRender: $badgeRender);
+    }
+
+    private function renderBadge($safe, ProfileViews $profileView): string
+    {
+        // dd(config(key: 'badge.default_label'));
+        return $this->badgeRenderService->renderBadgeWithCount(
+            label: $safe->label ?? config(key: 'badge.default_label'),
+            count: $profileView->getCount(username: $safe->username) ?? 0,
+            messageBackgroundFill: $safe->color ?? config(key: 'badge.default_color'),
+            badgeStyle: $safe->style ?? config(key: 'badge.default_style'),
+            abbreviated: $safe->abbreviated ?? config(key: 'badge.default_abbreviated'),
         );
+    }
 
-        return response($badgeRender, headers: [
+    private function createBadgeResponse(string $badgeRender): Response
+    {
+        return response(content: $badgeRender, status: 200, headers: [
             'Content-Type' => 'image/svg+xml',
-            'Cache-Control' => 'Cache-Control: max-age=0, no-cache, no-store, must-revalidate',
+            'Cache-Control' => 'max-age=0, no-cache, no-store, must-revalidate',
         ]);
     }
 }
