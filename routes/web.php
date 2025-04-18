b699c2d44f58ea9000ae54bc89207f764427c077
PA Ulander <pa@kabelkultur.se>
2025-04-18 12:46:50 +0200

    refactor rate limiting

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\ProfileViewsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ProfileViewsController::class, 'index']);
+Route::get('/', [ProfileViewsController::class, 'index'])
+    ->middleware('throttle:profile-views');
