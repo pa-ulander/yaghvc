d11e70e3007b9f9e8b59817be9cbb83844cc7676
PA Ulander <pa@kabelkultur.se>
2025-04-20 00:57:10 +0200

    add GithubCamoOnly middleware to web and api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,24 +1,27 @@
 <?php
 
 use App\Http\Middleware\ForceJson;
+use App\Http\Middleware\GithubCamoOnly;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(path: __DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(callback: function (Middleware $middleware): void {
         $middleware->web(append: [
             ForceJson::class,
+            GithubCamoOnly::class,
         ]);
 
         $middleware->api(prepend: [
             ForceJson::class,
+            GithubCamoOnly::class,
         ]);
     })
     ->withExceptions(using: function (Exceptions $exceptions): void {
