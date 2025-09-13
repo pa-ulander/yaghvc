42788fb6f918ec8acead43b19de48ff3637adb18
PA Ulander <pa@kabelkultur.se>
2025-09-13 08:32:32 +0200

    Refactor Pest setup

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,9 +11,7 @@
 |
 */
 
-pest()->extend(Tests\TestCase::class)
-    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature', 'E2E', 'Unit');
+uses(Tests\TestCase::class)->in('Feature', 'E2E', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
